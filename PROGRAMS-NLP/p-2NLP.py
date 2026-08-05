4fb4477321515e9ae4e83fa6bd7e89c806960383
Tharaknath-Reddy <tharakreddy399@gmail.com>
2026-08-05 21:49:29 +0530

    Delete PROGRAMS-NLP directory

--- PROGRAMS-NLP/p-2NLP.py
+++
@@ -1,8 +0,0 @@
-def finite_state_automaton(s):
-    if s.endswith("ab"):
-        return "Accepted"
-    else:
-        return "Rejected"
-
-string = input("Enter a string: ")
-print(finite_state_automaton(string))
--- PROGRAMS-NLP/p-4NLP.py
+++
@@ -1,9 +0,0 @@
-def plural(noun):
-    if noun.endswith("y"):
-        return noun[:-1] + "ies"
-    else:
-        return noun + "s"
-
-word = input("Enter a noun: ")
-print("Plural:", plural(word))
-
